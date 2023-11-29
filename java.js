7b385729715e86662feba203d4539110cb696f15
FikruDemlew <fikrudemlew@gmail.com>
2023-11-29 14:23:47 +0300

    update calculator

--- java.js
+++ java.js
@@ -1,8 +1,13 @@
+function calc() {
+
 const mass = prompt("what is your mass")
 
 const height = prompt("what is your height")
 
 const result = mass / (height * height)
 
+console.log('your bmi is'+ result);
+}
 
+calc();
 
