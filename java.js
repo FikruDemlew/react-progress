f845451c3625299e20099476868732cd7209639c
FikruDemlew <fikrudemlew@gmail.com>
2023-12-04 22:01:23 +0300

    Odd number generator function

--- java.js
+++ java.js
@@ -1,26 +1,18 @@
-const x = prompt('input Number')
 
-function odd(number) {
-for (let i = 0; i <= number; i++) {
-    if (i % 2 !== 0) {
-        console.log(i)
-    }
-}
-}
 
-odd(x)
-// const element = document.getElementById("add");
-// const number = document.getElementById("example")
-// console.log(element.innerHTML)
-// element.addEventListener("click", function(){
-//     let x = number.innerHTML
-//     x= Number(x)
-//  number.innerHTML=x + 1});
 
-// const subs = document.getElementById("sub")
+const element = document.getElementById("add");
+const number = document.getElementById("example")
+console.log(element.innerHTML)
+element.addEventListener("click", function(){
+    let x = number.innerHTML
+    x= Number(x)
+ number.innerHTML=x + 1});
 
-// subs.addEventListener("click", function(){
-//     let x = number.innerHTML
-//     x = Number(x)
-//     number.innerHTML = x - 1
-// });
+const subs = document.getElementById("sub")
+
+subs.addEventListener("click", function(){
+    let x = number.innerHTML
+    x = Number(x)
+    number.innerHTML = x - 1
+});
